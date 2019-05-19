fbcfb80db9a45b4a1c807c1d766e30b7653788fc
Jules Ponsaillé <jules.ponsaille@isep.fr>
2019-05-19 09:52:53 +0200

    sattelite speed

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,15 +2,18 @@ import * as p5 from 'p5';
 import constants from './constants';
 import Satellite from './satellite';
 
+let fps = 60;
+
 const width = window.innerWidth;
 const height = window.innerHeight;
-const earthSize = 10;
+const earthSize = 20;
 
 const ratio = math.divide(earthSize, constants.earthRadius);
 
 // Les variables initiales
 let perigee = math.add(math.bignumber('20180000'), constants.earthRadius);
-let apogee = math.add(perigee, math.bignumber('201800000'));
+let apogee = perigee;
+//let apogee = math.add(perigee, math.bignumber('201800000'));
 
 
 // Les variables initiales déduites
@@ -20,17 +23,20 @@ let e = math.divide(c, a);
 let b = math.sqrt(math.subtract(math.multiply(a, a), math.multiply(c,c)))
 
 
-let satellite = new Satellite(math.bignumber(0), perigee, apogee, math.bignumber(60));
+let satellite = new Satellite(perigee, apogee, math.bignumber((11*60 + 58)*60));
 
 window.setup = function () {
   createCanvas(width,height);
   background(0);
-  frameRate(30);
+  frameRate(fps);
 }
 
 window.draw = function () {
+  background(0);
   ellipse(width/2, height/2, math.number(math.multiply(2, math.multiply(a, ratio))), math.number(math.multiply(2, math.multiply(b, ratio))));
   ellipse(width/2-math.multiply(c, ratio), height/2, earthSize, earthSize);
-  let satellitePos = satellite.getPosition();
-  ellipse(math.multiply(satellitePos.x,ratio)*1+width/2-math.multiply(c, ratio), math.multiply(satellitePos.y,ratio)*1+height/2, earthSize, earthSize);
+  let satellitePos = satellite.getNextPosition(1/fps);
+  ellipse(math.multiply(satellitePos.x,ratio)*1+width/2, math.multiply(satellitePos.y,ratio)*1+height/2, earthSize, earthSize);
+  fill(255);
+  text(`Vsattelite = ${satellitePos.v*1}`, 10, 10);
 }
--- src/scripts/satellite.js
+++ src/scripts/satellite.js
@@ -1,6 +1,6 @@
 class Satellite {
-  constructor(theta, perigee, apogee, P) {
-    this.theta = theta;
+  constructor(perigee, apogee, P) {
+    this.theta = math.bignumber(0);
     this.perigee = perigee;
     this.apogee = apogee
     this.a = math.divide(math.add(perigee, apogee), 2);  
@@ -11,28 +11,46 @@ class Satellite {
     this.r = this.perigee;
     this.P = P;
     this.n = math.divide(math.multiply(math.bignumber(2), math.bignumber(Math.PI)), this.P);
+    this.x = math.multiply(math.sum(this.r), math.subtract(0,math.cos(this.theta)));
+    this.y = math.multiply(this.r, math.sin(this.theta));
+    this.mu = math.eval('(4 * ( pi ^ 2 ) * ( a ^3 ) ) / ( P ^ 2 )', {
+      P: this.P,
+      pi: math.bignumber(Math.PI),
+      a: this.a
+    });
   }
 
-  update() {
+  update(dt) {
     this.dtheta = math.eval('a * b * n * t / ( r * r )', {
       a: this.a,
       b: this.b,
       n: this.n,
       r: this.r,
-      t: math.bignumber(1/30)
+      t: dt
     })
     
     this.theta = math.sum(this.theta, this.dtheta);
     this.r = math.divide(this.p, math.add(math.bignumber(1), math.multiply(this.e, math.cos(this.theta))));
+    this.x = math.subtract(math.multiply(this.r, math.subtract(0,math.cos(this.theta))), this.c);
+    this.y = math.multiply(this.r, math.sin(this.theta));
+    this.v = math.eval('sqrt( mu * ( ( 2 / r ) - (1 / a ) ) )', {
+      mu: this.mu,
+      r: this.r,
+      a: this.a
+    });
+    console.log(this.r*this.dtheta/dt);
   }
 
-  getPosition() {
-    this.update();
-    let x = math.multiply(math.sum(this.r), math.subtract(0,math.cos(this.theta)));
-    let y = math.multiply(this.r, math.sin(this.theta));
+  getNextPosition(dt) {
+    dt = math.bignumber(dt);
+    let previousX = this.x;
+    let previousY = this.y;
+    this.update(dt);
+     
     return {
-      x,
-      y
+      x: this.x,
+      y: this.y,
+      v: this.v
     }
   }
 
